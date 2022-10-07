ae86fdc0f38f544b5ba5857f7a68ee12086301f4
Manfred Schneider <manfred.schneider@conciso.de>
2022-10-07 13:16:40 +0200

    Add hcloud server with configs

---
+++ create-server.tf
@@ -0,0 +1,14 @@
+
+resource "hcloud_server" "node1" {
+  name        = "demo-host-vm"
+  image       = "ubuntu-22.04"
+  server_type = "cpx11"
+  location    = "fsn1"
+  placement_group_id = hcloud_placement_group.my-placement-group.id
+  ssh_keys = [ "MSMacBook" ]
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  firewall_ids = [hcloud_firewall.hcloud1firewall.id, hcloud_firewall.hcloud2firewall.id]
+}
